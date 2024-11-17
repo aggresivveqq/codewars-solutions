fc0165eca1136ea16bf8e1d9492d714390aed542
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2024-11-17 20:23:28 +0500

    Create String repeat.cs

---
+++ C#/8 kyu/String repeat.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+namespace Solution
+{
+  public static class Program
+  {
+    public static string RepeatStr(int n, string s)
+    {
+             return string.Concat(Enumerable.Repeat(s,n));
+    }
+  }
+}
