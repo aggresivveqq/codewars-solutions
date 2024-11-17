59affe911b59960105c58ba3b7c29b930420bd71
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2024-11-17 20:24:05 +0500

    Create Remove String Spaces.cs

---
+++ C#/8 kyu/Remove String Spaces.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+namespace Solution 
+{
+  public static class SpacesRemover
+  {
+    public static string NoSpace(string input)
+    {
+   
+            return new string(input.Where(c => !char.IsWhiteSpace(c)).ToArray());
+    
+      }
+  }
+}
