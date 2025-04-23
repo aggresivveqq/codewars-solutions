a87851bc1e871206d47b09f37a25881ff5f251db
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2025-04-23 16:24:21 +0500

    Create Grasshopper - Ifelse syntax debug.cs

---
+++ C#/8kyu/Grasshopper - Ifelse syntax debug.cs
@@ -0,0 +1,34 @@
+public class Player
+{
+  private int health = 100;
+  public int Health
+  {
+    get
+    {
+      return this.health;
+    }
+    set
+    {
+      health = value;
+    }
+  }
+  
+  public Player()
+  {
+    
+  }
+  
+  public bool CheckAlive()
+  {
+    if(this.Health <= 0)
+    {
+      return false;
+    }
+    else if(this.Health > 0)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
