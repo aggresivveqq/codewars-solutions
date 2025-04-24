25d7b82a27aa4f914d7a698a18cf55aa7463c1cb
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2025-04-24 16:22:49 +0500

    Create  Calculate average.cs

---
+++ C#/8kyu/Calculate average.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    { 
+      List<double> qwe = array.ToList();
+      return qwe.Average();
+    }
+} 
