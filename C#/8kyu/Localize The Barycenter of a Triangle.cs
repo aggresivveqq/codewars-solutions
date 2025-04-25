53769462989f245cd5e2e81f17ef2f4dbe6d0ce9
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2025-04-25 16:19:47 +0500

    Create Localize The Barycenter of a Triangle.cs

---
+++ C#/8kyu/Localize The Barycenter of a Triangle.cs
@@ -0,0 +1,10 @@
+using System;
+public class Barycenter
+{
+	public static double[] BarTriang(double[] x, double[] y, double[] z)
+	{
+    double a = Math.Round((x[0] + y[0] + z[0]) / 3,4);
+    double b = Math.Round((x[1] + y[1] + z[1]) / 3,4);
+    return new double[] {a,b};
+  }
+}
